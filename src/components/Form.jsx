3336139a23fd7aab33e9f895b61390c9133c17c9
Sahil Suthar <sahil26dd@gmail.com>
2024-09-17 18:17:14 +0530

    initial files

---
+++ src/components/Form.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+// eslint-disable-next-line react/prop-types
+export default function Form({ onAddItem }) {
+  // Step 1
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      description: description,
+      quantity: quantity,
+      packed: false,
+      id: Date.now(),
+    };
+
+    onAddItem(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+
+      {/* Controlled element: Step 2 -> useValue of state, Step 3 -> setValue of state */}
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+
+      {/* Controlled element: Step 2 -> useValue of state, Step 3 -> setValue of state */}
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
